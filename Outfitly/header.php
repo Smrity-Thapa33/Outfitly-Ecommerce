4cf2920c3b260c8613a94d6fb92644076f0df13b
Smrity Thapa <thapasmrity6@gmail.com>
2026-02-05 22:07:07 +0545

    Initial commit

---
+++ Outfitly/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Outfitly: Homepage</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Montserrat&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div id="navbar">
+            <div id="logo">
+                <img src="assets/images/logo1.png" alt="logo image">
+            </div>
+            <div id="nav">
+                <a href="index.php">Home</a>
+                <a href="product.php">Products</a>
+                <a href="about.php">About</a>
+                <a href="contact.php">Contact</a>
+            </div>
+            <div id="login_register">
+                <a href="login.php">Login</a>
+                <a href="register.php">Register</a>
+            </div>
+        </div>
